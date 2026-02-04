6c38b9247e52f1b493a343e2c126e10a02133dcc
NISARGA NS <nisargans011@gmail.com>
2026-02-04 12:10:53 +0530

    Correct main method and input for rat population
    Fix method signature and update population input handling.

--- Java_Code/Conditional_Statements2.java
+++ Java_Code/Conditional_Statements2.java
@@ -14,7 +14,15 @@ class Main {
 // User given input for simple if 
 import java.util.Scanner ;
 class rat_population {
-    public static void min(string
+    public static void min(string[] args){
+    Scanner src = new Scanner(System.in)
+        System.out.println("Enter the expected  population of the rats :");
+        string population=Scanner.nextLine;
+        if(population >= 200){
+        System.out.println("Check out ! Population is increasing ");
+        }
+    }
+        
 
 
 
