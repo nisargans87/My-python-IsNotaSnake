38d3f025b8e444eb7c60256e9c44ccefd65b4b0f
NISARGA NS <nisargans011@gmail.com>
2026-02-04 07:58:10 +0530

    Add simple if statement demonstration for rat population

---
+++ Java_Code/Conditional_Statements2.java
@@ -0,0 +1,11 @@
+// Use any editor to run 
+//  simple if statement demonstration 
+class Main {
+    public static void main(String[] args) {
+        int rat_population = 24;
+        if(rat_population >=  20){
+        System.out.println("Check out ! Population is increasing ");
+    }
+}
+  }
+// code ends here 
