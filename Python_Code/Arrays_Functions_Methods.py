8f2510b2a334321db872f73d7c27377eb71c5dcb
NISARGA NS <nisargans011@gmail.com>
2026-02-06 08:48:13 +0530

    Update and rename Arrays_Functions_Methods to Arrays_Functions_Methods.py

---
+++ Python_Code/Arrays_Functions_Methods.py
@@ -0,0 +1,25 @@
+array creation using lists 
+array = [22,33,44,55]
+print(array)
+print(array[1])
+print(array[2])
+print(array[1],array[3])
+print(array[-1],array[3])
+print(array[0:3])
+print(array[0:1])
+# array creation and access through index numbers using tuples 
+array1=(22,66,24)
+print(array1[1])
+print(array1[1:2])
+print(array1[0:3])
+# array creation using sets 
+# TypeError: 'set' object is not subscriptable
+array2={13,14,15,16}
+print(array2)
+print(array2[2])
+# statements ends here 
+# "Arrays can be created using tuples and lists but not with sets ". Find out the reasons . 
+# This program is used to creation of an arrays using lists and tuples and also case study about sets 
+
+
+
