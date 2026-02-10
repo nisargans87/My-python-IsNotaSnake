4f4ecf8518b3836a013e37db376979631ec8714e
NISARGA NS <nisargans011@gmail.com>
2026-02-10 07:31:42 +0530

    Add Hello World C program
    Initial implementation of a C program to print 'HELLO ! World'.

---
+++ C_Code/Hello_World.c
@@ -0,0 +1,10 @@
+// Sample Program to print "HELLO ! World " using printf to display the text in the terminal 
+// Programming Structure is as follows 
+
+#include <stdio.h>
+int main () {
+  printf(" HELLO ! World " );
+return 0 ; 
+} 
+
+// Output - HELLO ! World 
