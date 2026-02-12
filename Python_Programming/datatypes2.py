d3bfa2a9e5c8cea136e0ea8ca6ca6424d51c1af8
NISARGA NS <nisargans011@gmail.com>
2026-02-12 20:16:59 +0530

    Update and rename DataTypes2.py to datatypes2.py

--- Python_Programming/datatypes2.py
+++ Python_Programming/datatypes2.py
@@ -3,52 +3,53 @@ name = "nisarga"
 print(name);
 print(type(name))
 # prints datatype of the name variable
+
 a = 20 
 print(a)
 print(type(a))
 # prints datatype of the a variable
+
 b = 23.4567
 print(b)
 print(type(b))
 # prints datatype of the b variable
+
 z = 2+3j 
 print(z)
 print(type(z))
 # prints datatype of the z variable
+
 c = True
 print(c)
 print(type(c))
 # prints datatype of the c variable
+
 d = 'A'
 print(d)
 print(type(d))
 # prints datatype of the d variable
+
 e = None
 print(e)
 print(type(e))
 # prints datatype of the e variable
+
 f = [ 2,3,4,5,6]
 print(f)
 print(type(f))
 # prints datatype of the f variable
+
 g = { 12.4,23.5,11.5}
 print(g)
 print(type(g))
 # prints datatype of the g variable
+
 h=(1.2,3.4,5.3)
 print(h)
 print(type(h))
 # prints datatype of the h variable
+
 i = {"num1":2 , "num2":4 , "num3":6}
 print(i)
 print(type(i))
 # prints datatype of the i variable
-
-
-
-  
-
-
-
-
-
