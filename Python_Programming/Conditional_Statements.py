e8d33e8c30a706bc285df4247ccd68b0f54965fa
NISARGA NS <nisargans011@gmail.com>
2026-02-12 20:24:37 +0530

    Rename conditional_statements.py to Conditional_Statements.py

--- Python_Programming/Conditional_Statements.py
+++ Python_Programming/Conditional_Statements.py
@@ -11,12 +11,13 @@ if (year % 400== 0) or (year % 4 == 0 and year % 100 != 0) :
     print("Given year , " , year, " is a leap year ")
 else :
     print("Given year , " , year ,"is not a leap year " )
-  
+
 # Output 
 ''' Enter year to find whether it is leap year or not :  1996
 Given year , 1996 is a leap year '''
 ''' Enter year to find whether it is leap year or not :  2026
 Given year , 2026 is a  not leap year '''
+
 
 # Usage of if..elif..else by taking the traffic colors from user and to predict the colors ' 
 print("---- Traffic Guidelines ----")
@@ -33,10 +34,10 @@ else :
  ''' Output   
 ---- Traffic Guidelines ----
 Enter the color to know the meaning : green 
-Move ! '''
+Move ! 
 ---- Traffic Guidelines ----
 Enter the color to know the meaning : yellow
-Get ready to go ! '''
+Get ready to go ! 
 ---- Traffic Guidelines ----
 Enter the color to know the meaning : blue
 Invalid  ! '''
