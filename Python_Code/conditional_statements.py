b8855e1304c44fb06ef85fcf2911fdc707181366
NISARGA NS <nisargans011@gmail.com>
2026-02-12 10:41:15 +0530

    Implement leap year checker using if..else
    Added a script to determine if a given year is a leap year using conditional statements.

--- Python_Code/conditional_statements.py
+++ Python_Code/conditional_statements.py
@@ -1,1 +1,20 @@
+# Usage of conditional statements if..else to find out given year is leap year or not
+year = int(input("Enter year to find whether it is leap year or not :  "))  
+if (year % 400== 0) or (year % 4 == 0 and year % 100 != 0) :
+    print("Given year , " , year, " is a leap year ")
+else :
+    print("Given year , " , year ,"is not a leap year " )
+  
+# Output 
+''' Enter year to find whether it is leap year or not :  1996
+Given year , 1996 is a leap year '''
+''' Enter year to find whether it is leap year or not :  2026
+Given year , 2026 is a  not leap year '''
+
+
+
+
+
+
+
 
