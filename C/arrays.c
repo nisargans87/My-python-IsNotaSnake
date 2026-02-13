d08ad6d52f459385fc4b1877423998a2f38f6bb5
NISARGA NS <nisargans011@gmail.com>
2026-02-13 09:59:51 +0530

    Update arrays.c

--- C/arrays.c
+++ C/arrays.c
@@ -1,3 +1,5 @@
+// Two dimensional Array 
+
 #include <stdio.h>
 int main() {
     int marks[3][3]={
@@ -29,6 +31,50 @@ S1 |85 |90 |78 |
 S2 |78 |94 |95 |
 S3 |56 |78 |34 |
 -------------- */
+
+
+
+// Three dimensional Array demonstration 
+#include <stdio.h>
+int main() {
+    int cube[2][3][3]={
+        {
+            {1,2,3},
+        {2,5,6},
+        {7,8,9}
+    },
+    {
+        {10,11,12},
+    {13,14,15},
+    {16,17,18}
+    }
+};
+printf("Displaying 3D array cube element:\n");
+for(int i=0;i<2;i++){
+    printf("\n---Layer%d--\n",i);
+    for(int j=0;j<3;j++){
+        for(int k=0;k<3;k++){
+            printf("%d\t",cube[i][j][k]);
+        }
+        printf("\n");
+    }
+}
+return 0;
+}
+
+/* Output 
+Displaying 3D array cube element:
+
+---Layer0--
+1	2	3	
+2	5	6	
+7	8	9	
+
+---Layer1--
+10	11	12	
+13	14	15	
+16	17	18	
+*/
       
 
 
