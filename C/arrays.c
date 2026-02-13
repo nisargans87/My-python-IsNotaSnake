59a318f452c9d5a1e1e2061a63577b00e0c8d581
NISARGA NS <nisargans011@gmail.com>
2026-02-13 09:33:13 +0530

    Create arrays.c

---
+++ C/arrays.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+    {85,90,78},
+    {78,94,95},
+    {56,78,34}
+};
+printf("students marks table\n");
+printf("----------\n");
+printf("Students|Sub1|Sub2|Sub3|\n");
+prinf("-----------\n");
+for(int i=0;i<3;i++){
+    print("S%d |", i+1);
+  for (int j=0;j<3;j++){
+    printf("%d |",marks[i][j]);
+  }
+  printf("\n");
+}
+prinf("--------------\n");
+return 0;
+  }
+
+
+
+
