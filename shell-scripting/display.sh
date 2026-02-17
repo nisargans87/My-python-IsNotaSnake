b821ec080bb3aa7723cbbd112ad0aa3a83148afc
NISARGA NS <nisargans011@gmail.com>
2026-02-17 10:06:01 +0530

    Create display.sh

---
+++ shell-scripting/display.sh
@@ -0,0 +1,43 @@
+# To display the given/assigned  value to the termimal
+
+#!/bin/bash
+# displaying  direct values 
+echo 10
+echo "Nisarga"
+echo 19.45
+echo 'A'
+echo true 
+echo none # prints none in online compiler 
+
+#!/bin/bash
+# declaring and then displaying 
+num1=200
+echo $num1
+name1="Nisarga"
+echo $name1
+alpha='A'
+echo $alpha 
+bool=true
+echo $bool 
+
+#!/bin/bash
+# declaring and then displaying at same time 
+num2=12.5
+num3=11.5
+num4=13.5
+echo $num2 $num3 $num4
+echo $num2,$num3,$num4
+echo $num2+$num3+$num4
+string1="My name is "
+string2="Nisarga"
+final_name = $string1$string2
+echo $final_name 
+# $string1+$string2 , string1,string2 - For this cases we will encounter different formats  of output 
+
+
+
+
+
+
+
+
