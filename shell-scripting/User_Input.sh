50c129235662d06d1933d3437f557ea3c4c7d5c1
NISARGA NS <nisargans011@gmail.com>
2026-02-17 13:55:35 +0530

    Create User_Input.sh

---
+++ shell-scripting/User_Input.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# example - 1
+echo " Enter a first number "
+read num1
+echo " Enter a second number "
+read num2
+echo "Enter a third number "
+read num3
+echo $num1 , $num2 , $num3
+
+# example - 2
+echo " Enter a first number "
+read name
+
+# example - 3 
+echo " Enter a first number "
+read name
+echo " Hii ! Welcome $name"
+
+
+
+
